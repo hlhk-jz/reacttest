7726de0db013168e29cb90306a4e95ad13eaee69
jiangzhi <zhi.jiang@zkang-e.com>
2023-11-23 17:31:20 +0800

    初始化

--- src/demo/Demo10.js
+++ src/demo/Demo10.js
@@ -1,35 +1,34 @@
 import { useMemo, useState,useEffect } from 'react'
-function CaclNum(count) {
-    console.log('重新渲染'+count)
-    let total = 0
-    for (let i = 0; i <= count; i++) {total += i }
-    return total
-}
 
 const Hook = () => {
     const [count, setCount] = useState(0)
     const [show, setShow] = useState(true)
 
     //useEffect 渲染之后执行，useMemo 在渲染之前执行
-    useEffect(()=>{
+   /* useEffect(()=>{
         console.log("useEffect执行了！！！"+count)
-    },[count]);
+    },[count]);*/
     /**
      * useMemo  为了性能优化存在的一个hook，把创建的函数和依赖项数组作为参数,
      * 它仅仅会在依赖项发生改变的时候，重新计算第一个参数的值，依赖不变时返回的值相同
+     * useMemo 和 useCallback区别是，useCallback缓存返回旧的函数，useMemo返回值
      */
-    const total =useMemo(()=>{
+    const total = useMemo(()=>{
         console.log("useMemo 执行了！！！"+count)
         console.log('重新渲染'+count)
         let total = 0
         for (let i = 0; i <= count; i++) {total += i }
         return total
     },[count])
-    /*const total =useMemo(()=>{
-            return CaclNum(count)
-        },[count])*/
-    //const total = CaclNum(count)
 
+  /*  const total = CaclNum(count)
+    function CaclNum() {
+        console.log('重新渲染'+count)
+        let total = 0
+        for (let i = 0; i <= count; i++) {total += i }
+        return total
+    }
+*/
     return (
         <>
             <p>计算数字之和{total}</p>
